17ab57fdf00ea336b926fa1ea81f0ed835653748
Noriyo Akita <noriyo.akita@gmail.com>
2017-05-25 12:20:51 +0000

    Enable to register first_name and last_name on signup

--- spec/features/users/signing_users_up_spec.rb
+++ spec/features/users/signing_users_up_spec.rb
@@ -5,6 +5,8 @@ RSpec.feature "Users::SigningUsersUp", type: :feature do
     visit '/'
     click_link 'Sign up'
 
+    fill_in 'First name', with: 'John'
+    fill_in 'Last name', with: 'Doe'
     fill_in 'Email', with: 'john@example.com'
     fill_in 'Password', with: 'password'
     fill_in 'Password confirmation', with: 'password'
@@ -12,5 +14,8 @@ RSpec.feature "Users::SigningUsersUp", type: :feature do
     click_button 'Sign up'
 
     expect(page).to have_content 'You have signed up successfully.'
+
+    visit '/'
+    expect(page).to have_content 'John Doe'
   end
 end

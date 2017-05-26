0d69aea8e7cef67a05898a4aa61a76912ba0f013
Noriyo Akita <noriyo.akita@gmail.com>
2017-05-26 12:11:05 +0000

    Generate Friendship model

---
+++ app/models/friendship.rb
@@ -0,0 +1,20 @@
+# == Schema Information
+#
+# Table name: friendships
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer
+#  friend_id  :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_friendships_on_friend_id  (friend_id)
+#  index_friendships_on_user_id    (user_id)
+#
+
+class Friendship < ApplicationRecord
+  belongs_to :user
+  belongs_to :friend, class_name: 'User'
+end
---
+++ spec/factories/friendships.rb
@@ -0,0 +1,22 @@
+# == Schema Information
+#
+# Table name: friendships
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer
+#  friend_id  :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_friendships_on_friend_id  (friend_id)
+#  index_friendships_on_user_id    (user_id)
+#
+
+FactoryGirl.define do
+  factory :friendship do
+    user nil
+    friend nil
+  end
+end

36f0c98b96ae190b459aa4c7ef0f94fa95a015c3
Noriyo Akita <noriyo.akita@gmail.com>
2017-06-01 12:12:33 +0000

    Generate Message model
    belongs_to user and room

---
+++ app/models/message.rb
@@ -0,0 +1,21 @@
+# == Schema Information
+#
+# Table name: messages
+#
+#  id         :integer          not null, primary key
+#  body       :text
+#  user_id    :integer
+#  room_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_messages_on_room_id  (room_id)
+#  index_messages_on_user_id  (user_id)
+#
+
+class Message < ApplicationRecord
+  belongs_to :user
+  belongs_to :room
+end
